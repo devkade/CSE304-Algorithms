95b44dbd292a3eb3be281c9c0d3fa8b5b4b685e3
DK Kang <devkade12@gmail.com>
2024-09-26 14:06:23 +0900

    solve quicksort, strassen

--- 2.6.quicksort.py
+++ 2.6.quicksort.py
@@ -3,53 +3,74 @@ def partition(low, high, S):
     j = low
 
     # Complete the code here
+    for i in range(low + 1, high + 1):
+        if S[i] < pivotitem:
+            j += 1
+            S[i], S[j] = S[j], S[i]
+
+    pivotpoint = j
+    S[low], S[pivotpoint] = S[pivotpoint], S[low]
+    return pivotpoint
+
 
 def quicksort(low, high, S):
     if low < high:
         # Complete the code here
-        
+        pivotpoint = partition(low, high, S)
+        quicksort(low, pivotpoint - 1, S)
+        quicksort(pivotpoint + 1, high, S)
+
+
 # Example 1 for 'partition()'
 print("######Example 1 for 'partition()'######")
-S = [17, 19, 39, 41, 73] # sorted in ascending order
+S = [17, 19, 39, 41, 73]  # sorted in ascending order
 pivotpoint = partition(0, len(S) - 1, S)
-print(f"Left Sub-S: {S[:pivotpoint]}, pivotitem={S[pivotpoint]}, Right Sub-S: {S[pivotpoint+1:]}")
+print(
+    f"Left Sub-S: {S[:pivotpoint]}, pivotitem={S[pivotpoint]}, Right Sub-S: {S[pivotpoint+1:]}"
+)
 print(f"{'-'*20}\n")
 
 # Example 2 for 'partition()'
 print("######Example 2 for 'partition()'######")
-S = [73, 41, 39, 19, 17] # sorted in descending order
+S = [73, 41, 39, 19, 17]  # sorted in descending order
 pivotpoint = partition(0, len(S) - 1, S)
-print(f"Left Sub-S: {S[:pivotpoint]}, pivotitem={S[pivotpoint]}, Right Sub-S: {S[pivotpoint+1:]}")
+print(
+    f"Left Sub-S: {S[:pivotpoint]}, pivotitem={S[pivotpoint]}, Right Sub-S: {S[pivotpoint+1:]}"
+)
 print(f"{'-'*20}\n")
 
 # Example 3 for 'partition()'
 print("######Example 3 for 'partition()'######")
-S = [39, 41, 19, 74, 17] # not sorted
+S = [39, 41, 19, 74, 17]  # not sorted
 pivotpoint = partition(0, len(S) - 1, S)
-print(f"Left Sub-S: {S[:pivotpoint]}, pivotitem={S[pivotpoint]}, Right Sub-S: {S[pivotpoint+1:]}")
+print(
+    f"Left Sub-S: {S[:pivotpoint]}, pivotitem={S[pivotpoint]}, Right Sub-S: {S[pivotpoint+1:]}"
+)
 print(f"{'-'*20}\n")
 
 # Example 4 - Your Custom Case for 'partition()'
-print("######Example 4 for 'partition()'######") 
+print("######Example 4 for 'partition()'######")
 # Insert your example here
-# S = 
+S = [123, 149, 44, 111, 79, 53]
 pivotpoint = partition(0, len(S) - 1, S)
-raise NotImplementedError("Complete your example.")
-print(f"Left Sub-S: {S[:pivotpoint]}, pivotitem={S[pivotpoint]}, Right Sub-S: {S[pivotpoint+1:]}")
+# raise NotImplementedError("Complete your example.")
+print(
+    f"Left Sub-S: {S[:pivotpoint]}, pivotitem={S[pivotpoint]}, Right Sub-S: {S[pivotpoint+1:]}"
+)
 print(f"{'-'*20}\n")
 
 # Example 1 for 'quicksort()'
 print("######Example 1 for 'quicksort()'######")
 S = [22, 98, 17, 16, 19, 73, 94, 41, 39, 66]
-quicksort(0, len(S)-1, S)
+quicksort(0, len(S) - 1, S)
 print("S:", S)
 print(f"{'-'*20}\n")
 
 # Example 2 - Your Custom Case for 'quicksort()'
-print("######Example 2 for 'quicksort()'######") 
+print("######Example 2 for 'quicksort()'######")
 # Insert your example here
-# S = 
-raise NotImplementedError("Complete your example.")
-quicksort(0, len(S)-1, S)
+S = [43, 52, 125, 54, 37, 110, 111, 103]
+# raise NotImplementedError("Complete your example.")
+quicksort(0, len(S) - 1, S)
 print("S:", S)
 print(f"{'-'*20}\n")
--- 2.8.strassen.py
+++ 2.8.strassen.py
@@ -1,9 +1,8 @@
 class Matrix:
-    
     def __init__(self, mat):
         self.n = len(mat)
         self.matrix = mat
-    
+
     def __add__(self, other):
         mat = [[0] * self.n for _ in range(self.n)]
         for i in range(self.n):
@@ -25,7 +24,8 @@ class Matrix:
                 for k in range(self.n):
                     mat[i][j] += self.matrix[i][k] * other.matrix[k][j]
         return Matrix(mat)
-            
+
+
 def partition(n, M):
     m = n // 2
     m1 = [[0] * m for _ in range(m)]
@@ -35,18 +35,28 @@ def partition(n, M):
     for i in range(m):
         for j in range(m):
             # Complete the code here
+            m1[i][j] = M.matrix[i][j]
+            m2[i][j] = M.matrix[i][j + m]
+            m3[i][j] = M.matrix[i + m][j]
+            m4[i][j] = M.matrix[i + m][j + m]
 
     return Matrix(m1), Matrix(m2), Matrix(m3), Matrix(m4)
-    
+
+
 def combine(n, M1, M2, M3, M4):
     m = n // 2
     mat = [[0] * n for _ in range(n)]
     for i in range(m):
         for j in range(m):
             # Complete the code here
+            mat[i][j] = M1.matrix[i][j]
+            mat[i][j + m] = M2.matrix[i][j]
+            mat[i + m][j] = M3.matrix[i][j]
+            mat[i + m][j + m] = M4.matrix[i][j]
 
     return Matrix(mat)
-   
+
+
 def strassen(n, A, B):
     global threshold
 
@@ -55,10 +65,24 @@ def strassen(n, A, B):
     else:
         A11, A12, A21, A22 = partition(n, A)
         B11, B12, B21, B22 = partition(n, B)
-        
+
         # Complete the code here
+        m = n // 2
+        M1 = strassen(m, A11 + A22, B11 + B22)
+        M2 = strassen(m, A21 + A22, B11)
+        M3 = strassen(m, A11, B12 - B22)
+        M4 = strassen(m, A22, B21 - B11)
+        M5 = strassen(m, A11 + A12, B22)
+        M6 = strassen(m, A21 - A11, B11 + B12)
+        M7 = strassen(m, A12 - A22, B21 + B22)
+
+        C11 = M1 + M4 - M5 + M7
+        C12 = M3 + M5
+        C21 = M2 + M4
+        C22 = M1 + M3 - M2 + M6
 
         return combine(n, C11, C12, C21, C22)
+
 
 threshold = 1
 
@@ -89,10 +113,10 @@ print(f"{'-'*20}\n")
 
 # Example 2 - Your Custom Case for 'partition() and combine()'
 print("######Example 2 for 'partition() and combine()'######")
-# n = 
-# M =
-assert n >= 4
-raise NotImplementedError("Complete your example.")
+n = 4
+M = Matrix([[2, 1, 1, 2], [1, 2, 2, 1], [4, 3, 3, 4], [3, 4, 4, 3]])
+# assert n >= 4
+# raise NotImplementedError("Complete your example.")
 M11, M12, M21, M22 = partition(n, M)
 combined_M = combine(n, M11, M12, M21, M22)
 
@@ -130,20 +154,21 @@ for i in range(C.n):
 print(f"{'-'*20}\n")
 
 # Example 2 - Your Custom Case for 'strassen()'
-print("######Example 2 for 'strassen()'######") 
+print("######Example 2 for 'strassen()'######")
 # Insert your example here
-# n = 
-# A = 
-# B = 
-assert n >= 4
+n = 4
+A = Matrix([[2, 1, 1, 2], [1, 2, 2, 1], [4, 3, 3, 4], [3, 4, 4, 3]])
+B = Matrix([[3, 0, 3, 0], [0, 2, 0, 2], [0, 1, 1, 0], [4, 0, 0, 4]])
+# assert n >= 4
 
 C = A * B
 print("Standard Matrix Multiplication:")
 for i in range(C.n):
     print(C.matrix[i])
-raise NotImplementedError("Complete your example.")
+# raise NotImplementedError("Complete your example.")
 C = strassen(n, A, B)
 print("Strassen Matrix Multiplication:")
 for i in range(C.n):
     print(C.matrix[i])
 print(f"{'-'*20}\n")
+
