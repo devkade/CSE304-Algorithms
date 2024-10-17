706e6a6473ff3849e4cc188300fccc6d0dac22ec
DK Kang <devkade12@gmail.com>
2024-10-17 13:37:41 +0900

    solve 4.1.prim, 4.3.dijkstra

--- 4.1.prim.py
+++ 4.1.prim.py
@@ -1,26 +1,42 @@
 def prim(n, W):
     F = []
+    vnear = 0
     for i in range(2, n + 1):
         nearest[i] = 1
         distance[i] = W[1][i]
     for _ in range(n - 1):
         # Complete the code here
+        minVal = INF
+        for i in range(2, n + 1):
+            if 0 <= distance[i] < minVal:
+                minVal = distance[i]
+                vnear = i
+
+        edge = (nearest[vnear], vnear, W[nearest[vnear]][vnear])
+        F.append(edge)
+        distance[vnear] = -1
+
+        for i in range(2, n + 1):
+            if W[i][vnear] < distance[i]:
+                distance[i] = W[i][vnear]
+                nearest[i] = vnear
 
     return F
-                
+
+
 INF = float("inf")
 
 # Example 1
 print("######Example 1######")
-n = 5 # the number of vertices
-W = [ # adjacency matrix
-        [INF,    INF,    INF,    INF,    INF,     INF],
-        [INF,      0,      1,      3,    INF,     INF], 
-        [INF,      1,      0,      3,      6,     INF],
-        [INF,      3,      3,      0,      4,       2],
-        [INF,    INF,      6,      4,      0,       5],
-        [INF,    INF,    INF,      2,      5,       0]
-    ]
+n = 5  # the number of vertices
+W = [  # adjacency matrix
+    [INF, INF, INF, INF, INF, INF],
+    [INF, 0, 1, 3, INF, INF],
+    [INF, 1, 0, 3, 6, INF],
+    [INF, 3, 3, 0, 4, 2],
+    [INF, INF, 6, 4, 0, 5],
+    [INF, INF, INF, 2, 5, 0],
+]
 nearest = [-1] * (n + 1)
 distance = [-1] * (n + 1)
 
@@ -28,12 +44,19 @@ F = prim(n, W)
 print(F)
 
 # # Example 2 - Your Custom Case
-# print("######Example 2 #######") 
+print("######Example 2 #######")
 # # Insert your example here
-# n =   
-# W = 
+n = 5
+W = [  # adjacency matrix
+    [INF, INF, INF, INF, INF, INF],
+    [INF, 0, 3, 1, 4, INF],
+    [INF, 3, 0, 7, 3, INF],
+    [INF, 1, 7, 0, 4, 2],
+    [INF, 4, 3, 4, 0, 5],
+    [INF, INF, INF, 2, 5, 0],
+]
 nearest = [-1] * (n + 1)
 distance = [-1] * (n + 1)
-raise NotImplementedError("Complete your example.")
+# raise NotImplementedError("Complete your example.")
 F = prim(n, W)
 print(F)
--- 4.3.dijkstra.py
+++ 4.3.dijkstra.py
@@ -5,38 +5,61 @@ def dijkstra(n, W):
         length[i] = W[1][i]
     for _ in range(n - 1):
         # Complete the code here
+        minVal = INF
+        # i vertex에서 가장 가까운 정점 뽑기
+        for i in range(2, n + 1):
+            if 0 <= length[i] < minVal:
+                minVal = length[i]
+                vnear = i
+
+        edge = (touch[vnear], vnear, W[touch[vnear]][vnear])
+        F.append(edge)
+        length[vnear] = -1
+
+        for i in range(2, n + 1):
+            if length[vnear] + W[vnear][i] < length[i]:
+                length[i] = length[vnear] + W[vnear][i]
+                touch[i] = vnear
 
     return F
-                
+
+
 INF = float("inf")
 
 # Example 1
 print("######Example 1######")
-n = 5 # the number of vertices
-W = [ # adjacency matrix
-        [INF,    INF,      INF,    INF,      INF,      INF],
-        [INF,      0,        7,      4,        6,        1],
-        [INF,    INF,        0,     INF,     INF,      INF],
-        [INF,    INF,        2,      0,        5,      INF],
-        [INF,    INF,        3,    INF,        0,      INF],
-        [INF,    INF,      INF,    INF,        1,        0]
-    ]
+n = 5  # the number of vertices
+W = [  # adjacency matrix
+    [INF, INF, INF, INF, INF, INF],
+    [INF, 0, 7, 4, 6, 1],
+    [INF, INF, 0, INF, INF, INF],
+    [INF, INF, 2, 0, 5, INF],
+    [INF, INF, 3, INF, 0, INF],
+    [INF, INF, INF, INF, 1, 0],
+]
 touch = [-1] * (n + 1)
 length = [-1] * (n + 1)
 
 F = dijkstra(n, W)
 print(F)
-
 
 
 # # Example 2 - Your Custom Case
-# print("######Example 2 #######") 
+print("######Example 2 #######")
 # # Insert your example here
-# n =   
-# W = 
+n = 5
+W = [  # adjacency matrix
+    [INF, INF, INF, INF, INF, INF],
+    [INF, 0, 6, 1, 4, INF],
+    [INF, 1, 0, 7, 1, INF],
+    [INF, 4, 1, 0, 4, 2],
+    [INF, INF, 1, 2, 0, 5],
+    [INF, INF, INF, 2, 5, 0],
+]
 touch = [-1] * (n + 1)
 length = [-1] * (n + 1)
-raise NotImplementedError("Complete your example.")
+# raise NotImplementedError("Complete your example.")
 
 F = dijkstra(n, W)
 print(F)
+
